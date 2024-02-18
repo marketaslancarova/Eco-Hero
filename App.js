c518fa5a523482a84a8c9d64b2af733d6ef21d2a
Markéta Šlancarová <marketaslancarova@gmail.com>
2024-02-18 21:29:50 +0100

    Update fonts and UI in App.js

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, Image, Button } from "react-native";
+import {
+  useFonts,
+  Mulish_400Regular,
+  Mulish_700Bold,
+} from "@expo-google-fonts/mulish";
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    Mulish_700Bold,
+    Mulish_400Regular,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontFamily: "Mulish_700Bold", fontSize: 24 }}>
+        Eco Hero
+      </Text>
+      <Text style={{ fontFamily: "Mulish_700Bold", fontSize: 24 }}>
+        Guardians of the Green!
+      </Text>
+      <Image source={require("./assets/eco-hero.png")} />
+      <Text style={{ fontFamily: "Mulish_400Regular", fontSize: 24 }}>
+        It's time to be a hero for our planet! Help Eco Hero in the epic fight
+        against Poluto and learn how to keep our world thriving.
+      </Text>
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert("Simple Button pressed")}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/welcome-page.js
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+
+const WelcomePage = () => {
+  return (
+    <View>
+      <Text
+        style={{
+          fontFamily: "Mulish",
+        }}
+      >
+        Druhá stránka
+      </Text>
+    </View>
+  );
+};
+
+export default WelcomePage;
